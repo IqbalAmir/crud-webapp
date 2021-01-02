e47f630d687266478daf1560d9732291395bdea8
IqbalAmir <u1751546@unimail.hud.ac.uk>
2021-01-02 20:56:13 +0000

    Implemented footer on all pages

--- resources/views/stadium/show.blade.php
+++ resources/views/stadium/show.blade.php
@@ -23,5 +23,11 @@
             </button>
         </form>
     @endcan
+    <footer class="bg-gray-200">
+        <div class="container mx-auto px-6 py-3 flex justify-between items-center">
+            <a href="#" class="text-xl font-bold text-gray-500 hover:text-gray-400">Stadium Tracker</a>
+            <p class="py-2 text-gray-500 sm:py-0">All rights reserved</p>
+        </div>
+    </footer>
 @endsection
 
